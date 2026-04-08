37896167cfe190b08a66b305d6c5535fe5e93923
Runbin Zhou <runbin1000@gmail.com>
2026-04-07 21:57:20 -0400

    fix import to main entry

--- src/middleware/ingestionAuth.middleware.ts
+++ src/middleware/ingestionAuth.middleware.ts
@@ -1,8 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import axios, { AxiosError }               from 'axios';
-import { hashApiKey }                      from '@team-deepiri/shared-utils/src/cryptoUtils';
-import { createRedisClient }               from '@team-deepiri/shared-utils/src/redisClient';
-import { ApiKeyCachePayload, ApiKeyScope } from '@team-deepiri/shared-utils/src/types';
+import { hashApiKey, createRedisClient, ApiKeyCachePayload, ApiKeyScope } from '@team-deepiri/shared-utils';
 
 const redis = createRedisClient();
 
